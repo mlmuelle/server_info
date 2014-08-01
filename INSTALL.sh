11546963d52082f706bc675798600337f07d22af
mlmuelle <mlmuelle@mtu.edu>
2014-08-01 12:21:27 -0400

    initial commit

---
+++ INSTALL.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir /home/mlmuelle/bin/backup
+chmod +x liinfo
+chmod +x sys.info.sh
+
+cp liinfo /home/mlmuelle/bin/backup
+cp sys.info /home/mlmuelle/bin/backup
+cp sys.info.sh /home/mlmuelle/bin/backup
+cp sys.info.sh /home/mlmuelle/Desktop
+
+chmod +x liinfo
+chmod +x sys.info.sh
---
+++ sys.info.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+/root/bin/backup/liinfo
+
+#sleep 1 && wmctrl -a Information -b add,above ***need to install wmctrl to use
+zenity --text-info --title="CPU Info" --filename=/root/bin/backup/sys.info --wi$
+
+
