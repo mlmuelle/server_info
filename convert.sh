4b0b047223bab2a5ea67bfe92dfd0297a2d38aad
Matthew Mueller <mlmuelle@mtu.edu>
2014-08-08 08:25:54 -0400

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -5,3 +5,5 @@
 TEXT=$(cat /path/to/sys.info)
 
 convert -pointsize 36 -draw "text 240,260 '$TEXT'" background.png background2.png
+
+#Look up all the different optoins that you want to alter teh image text
